53d2161138ad206a8bdb6f75ba66ffd972a42d90
Clark <clarkipeng@gmail.com>
2024-07-04 13:05:59 -0700

    commented RL-related code

--- endpoints/data_parser.py
+++ endpoints/data_parser.py
@@ -10,18 +10,30 @@ class DataParser(object):
     Parses the input data photos and assigns their file locations to a dictionary for later access
     """
 
-    def __init__(self, data_fp):
-        metadata_fp = os.path.join(data_fp, "consolidated_metadata.csv")
+    def __init__(self, data_fp, metadata_fn = "consolidated_metadata.csv"):
+        """
+        takes in a row of a pandas dataframe and returns the class of the humanoid in the dataframe
+
+        data_fp : location of the folder in which the metadata csv file is located
+        metadata_fn : name of the metadata csv file
+        """
+        metadata_fp = os.path.join(data_fp, metadata_fn)
         self.fp = data_fp
         self.df = pd.read_csv(metadata_fp)
         self.unvisited = self.df.index.to_list()
         self.visited = []
 
     def reset(self):
+        """
+        reset list of humanoids
+        """
         self.unvisited = self.df.index.to_list()
         self.visited = []
 
     def get_random(self):
+        """
+        gets and returns a random humanoid object (without replacement)
+        """
         if len(self.unvisited) == 0:
             raise ValueError("No humanoids remain")
         # index = random.randint(0, (len(self.unvisited)-1))  # Technically semirandom
@@ -41,6 +53,11 @@ class DataParser(object):
 
 # can be customized
 def datarow_to_state(datarow):
+    """
+    takes in a row of a pandas dataframe and returns the class of the humanoid in the dataframe
+
+    datarow : row of the metadata dataframe
+    """
     img_path = datarow['Filename']
     img_class = datarow['Class']
     img_injured = datarow['Injured']
