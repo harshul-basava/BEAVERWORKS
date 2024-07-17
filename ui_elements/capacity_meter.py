b8cdc4ade2e96284df64fb0739c12b00362d0c56
Harshul <harshul.basava@gmail.com>
2024-07-17 16:55:18 -0700

    Created ToolTips
    Created placeholder ToolTips for the capacity meter to show some information when holding a humanoid. Updated the drawn rectangles to be buttons in preparation for swapping.

--- ui_elements/capacity_meter.py
+++ ui_elements/capacity_meter.py
@@ -1,13 +1,18 @@
 import math
 import tkinter as tk
+from tkmacosx import Button
+
+# TODO - need to install: pip install tkinter-tooltip
+from tktooltip import ToolTip
 
 
 class CapacityMeter(object):
     def __init__(self, root, w, h, max_cap):
+        self.root = root
         self.canvas = tk.Canvas(root, width=math.floor(0.2 * w), height=math.floor(0.3 * h))
         self.canvas.place(x=math.floor(0.75 * w), y=math.floor(0.4 * h))
         self.__units = []
-        self.unit_size = 40
+        self.unit_size = 34  # resized in order to make two rows of 5
         self.canvas.update()
         self.render(max_cap, self.unit_size)
 
@@ -25,11 +30,21 @@ class CapacityMeter(object):
 
     def update_fill(self, index):
         if index != 0:
-            self.canvas.itemconfig(self.__units[index-1], stipple="")
+            # self.root.itemconfig(self.__units[index-1], fill="midnightblue", activefill='red', stipple="")
+            curr = self.__units[index-1]
+            curr.config(bg="midnightblue", state="normal", activebackground='midnightblue')  # changing the color of the button and adding tooltip
+            ToolTip(curr, msg="important info")
+
         else:
             for unit in self.__units:
-                self.canvas.itemconfig(unit, stipple="gray25")
+                # self.root.itemconfig(unit, fill="white", activefill='white', stipple="gray25")
+                unit.config(bg="white", state="disabled", activebackground='white')
+                unit.unbind("<Enter>")  # removing the tooltip from the button when empty
+                unit.unbind("<Leave>")
 
 
 def create_unit(canvas, x, y, size):
-    return canvas.create_rectangle(x, y, x+size, y+size, fill='midnightblue', stipple="gray25")
+    bn = Button(canvas, bg='white', height=size, width=size, state="disabled", activebackground='white')
+    bn.place(x=x, y=y)  # replaced the rectangle with a button
+    bn.pack
+    return bn  # canvas.create_rectangle(x, y, x+size, y+size, fill='white', stipple="gray25")
