188d687e6e23ef41e8d0bbbfdebb72a90e4769c2
Moksh Shah <mokshpshah1@gmail.com>
2024-07-22 10:22:58 -0500

    fixed reveal

--- ui_elements/probability.py
+++ ui_elements/probability.py
@@ -6,23 +6,43 @@ class Probability(object):
     def __init__(self, root, w, h, prob):
         self.canvas = tk.Canvas(root, width=math.floor(0.2 * w), height=math.floor(0.3 * h))
         self.canvas.place(x=math.floor(0.75 * w), y=math.floor(0.6 * h))
+        self.jobs=[]
+        self.probab=[]
         self.render(prob)
         self.canvas.update()
+        
 
 
     def render_Job(self, job):
+        if len(self.jobs)>0:
+            for jobb in self.jobs:
+                jobb.destroy()
+        if len(self.probab)>0:
+            for pro in self.probab:
+                pro.destroy()
+        self.jobs.append(tk.Label(self.canvas, text="Job: " + job, font=("Arial", 15)))
+        self.jobs[len(self.jobs)-1].place(x=80, y=30)
         
-        tk.Label(self.canvas, text="Job", font=("Arial", 15)).place(x=80, y=30)
-        tk.Label(self.canvas, text=job, font=("Arial", 15)).place(x=80, y=50)
         self.canvas.update()
         return
     def render(self, probs):
+        if len(self.jobs)>0:
+            for job in self.jobs:
+                job.destroy()
+        if len(self.probab)>0:
+            for pro in self.probab:
+                pro.destroy()
 
-        tk.Label(self.canvas, text="Probability", font=("Arial", 15)).place(x=80, y=30)
+        self.probab.append(tk.Label(self.canvas, text="Probability", font=("Arial", 15)))
+        self.probab[len(self.probab)-1].place(x=80, y=30)
         i=0
         for job, prob in probs.items():
-            tk.Label(self.canvas, text=job, font=("Arial", 10)).place(x=70, y=80 + 30 * i)
-            tk.Label(self.canvas, text=str(prob)+"%", font=("Arial", 10)).place(x=160, y=80 + 30 * i)
+            self.probab.append(tk.Label(self.canvas, text=job, font=("Arial", 10)))
+            self.probab[len(self.probab)-1].place(x=70, y=80+30*i)
+
+            
+            self.probab.append(tk.Label(self.canvas, text=str(prob)+"%", font=("Arial", 10)))
+            self.probab[len(self.probab)-1].place(x=160, y=80 + 30 * i)
             i+=.8
         return
       
