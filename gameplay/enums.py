a5067086fdd5140a6511237f2a97eaedc7587897
Charlotte Zhou <charlottezhou6@gmail.com>
2024-07-25 10:54:47 -0700

    adjusting enum value

--- gameplay/enums.py
+++ gameplay/enums.py
@@ -13,11 +13,11 @@ class ActionState(Enum):
     REVEAL = 'reveal'
     
 class ActionCost(Enum):
-    SAVE = 15
-    SQUISH = 5
-    SKIP = 10
-    SCRAM = 45
-    REVEAL = 15
+    SAVE = 30
+    SQUISH = 15
+    SKIP = 20
+    SCRAM = 70
+    REVEAL = 30
 
 class Job(Enum):
     DOCTOR = "doctor"
@@ -25,8 +25,9 @@ class Job(Enum):
     THUG = "thug"
     NORMAL = "normal"
     FATTY = "fatty"
+    PESSIMIST = "pessimist"
 
 class JobBaseEffect(Enum):
-    ENGINEER = 20
-    FATTY = 30
+    ENGINEER = 45
+    FATTY = 60
     PESSIMIST = 0.4
